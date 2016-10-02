6a70681e7a938e39afbc9594bf8336b0567fa3a9
andreagomez95 <andreittttta@gmail.com>
2016-10-02 11:58:44 -0600

    Cambios en frame y en cliente

--- src/actors/Client.java
+++ src/actors/Client.java
@@ -4,6 +4,9 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
+//import java.util.AbstractQueue;
+import java.util.ArrayDeque;
+import java.util.Queue;
 
 import javax.swing.JOptionPane;
 
@@ -12,25 +15,34 @@ import javax.swing.JOptionPane;
  */
 public class Client {
 
-		public static int numServerSocket=9093;
-		public static String filePath = "C:/Users/DELL/Documents/GitHub/TP1_Redes/datosTP1.txt";
 		public static FileHandler fh = new FileHandler();
-		public static Socket serverSocket;// = new Socket(serverAddress, numServerSocket);
-		public static int numElemVentana;
-		public static int num1eroVentana;
+		public static String filePath = "C:/Users/DELL/Documents/GitHub/TP1_Redes/datosTP1.txt";
+
+		public static Socket serverSocket; // = new Socket(serverAddress, numServerSocket);
+		public static int numServerSocket=9093; //Usuario debe poder meterlo
+		public static int numElemVentana; //Usuario debe poder meterlo
+		public static int num1eroVentana; //Usuario debe poder meterlo
+		
+		public Queue<Frame> colaPendientes = new ArrayDeque<Frame>();
+		public Queue<Frame> colaVentana = new ArrayDeque<Frame>();
 	    /**
 	     * Runs the client as an application.  First it displays a dialog
 	     * box asking for the IP address or hostname of a host running
 	     * the date server, then connects to it and displays the date that
 	     * it serves.
 	     */
+		public static void ponerDatosEnFrames(String datos){
+			
+		}
 		
 		public static void main(String[] args) throws IOException {
 			String datosEntrada = fh.readUsingBuffer(filePath);
-	        //String serverAddress = "hola";
-			String serverAddress = "Prueba";//JOptionPane.showInputDialog("Enter IP Address of a machine that is\n" + "running the date service on port 9090:");
-					
-					
+	        
+			String serverAddress = "localhost";//Mejor mantenerlo en local host para que funcione bien.
+			//=JOptionPane.showInputDialog("Enter IP Address of a machine that is\n" + "running the date service on port 9090:");
+			//String serverAddress = "hola";
+			
+			ponerDatosEnFrames(datosEntrada);
 	        System.out.println("Client connecting to server at " + serverAddress + " in port 9091");
 	        /*Socket*/ serverSocket = new Socket(serverAddress, numServerSocket);
 	        System.out.println("Client receiving package from server.");
--- src/actors/Frame.java
+++ src/actors/Frame.java
@@ -4,21 +4,49 @@ import java.util.Date;
 
 public class Frame
 {
-
-	private boolean recibido;
+	//public static long infinito=Long.MAX_VALUE;
+	private static boolean recibido;
 	
-	private long timeout;
+	private static long timeout;
 	
-	private int idFrame;
+	private static int idFrame;
 	
-	private char data;
+	private static char data;
 	
-	public Frame(int id, long time) 
+	public Frame(int id) 
 	{
-		recibido = false;
-		timeout = time;
 		idFrame = id;
 		data = ' ';
+		recibido = false;
+		timeout = Long.MIN_VALUE;
+	}
+	
+	public static void setIdFrame(int dato){
+		idFrame= dato;
+	}
+	public static int getIdFrame(){
+		return idFrame;
+	}
+	
+	public static void setData(char dato){
+		data= dato;
+	}
+	public static char getData(){
+		return data;
+	}
+	
+	public static void setRecibido(boolean dato){
+		recibido= dato;
+	}
+	public static boolean getRecibido(){
+		return recibido;
+	}
+	
+	public static void setTimeout(long dato){
+		timeout= dato;
+	}
+	public static long getTimeout(){
+		return timeout;
 	}
 
 }
